15b008077364e5194a99574dee209e3cde34f68b
Schiefer, Ben <Ben.Schiefer@yeslifecyclemarketing.com>
2019-01-09 11:07:54 -0600

    move index to root

---
+++ index.js
@@ -0,0 +1,13 @@
+import * as colors from './src/theme/colors';
+import * as reportTheme from './src/theme/report-theme';
+import * as constants from './src/theme/theme.constants';
+import * as theme from './src/theme/theme';
+
+module.exports = {
+    theme: {
+        colors,
+        reportTheme,
+        constants,
+        theme
+    }
+};
