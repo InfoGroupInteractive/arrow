dd75a075a89cf261f67ece254e333b7cdb1876b9
Schiefer, Ben <Ben.Schiefer@yeslifecyclemarketing.com>
2019-01-09 10:47:04 -0600

    adding in theme

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as colors from './theme/colors';
+import * as reportTheme from './theme/report-theme';
+import * as constants from './theme/theme.constants';
+import * as theme from './theme/theme';
+
+export const {
+    theme: {
+        colors,
+        reportTheme,
+        constants,
+        theme
+    }
+};
---
+++ src/theme/theme.js
@@ -0,0 +1,72 @@
+import * as colors from './colors';
+
+const font = {
+    family: '"Open Sans", sans-serif'
+};
+
+const glowbal = {
+    font: font,
+    input: {
+        weight: 400
+    },
+    breakpoints: {
+        medium: {
+            value: 1200
+        }
+    }
+};
+
+const anchor = {
+    fontWeight: 400
+};
+
+const size = {
+    xxsmall: '48px',
+    xsmall: '96px',
+    small: '192px',
+    medium: '384px',
+    large: '576px',
+    xlarge: '768px',
+    xxlarge: '1152px',
+    full: '100%'
+};
+
+const edgeSize = {
+    none: '0px',
+    hair: '1px',
+    xxsmall: '3px',
+    xsmall: '18px',
+    small: '12px',
+    medium: '24px',
+    large: '48px',
+    xlarge: '96px',
+    responsiveBreakpoint: 'small'
+};
+
+export const dark = {
+    global: Object.assign({}, glowbal, {
+        colors: colors.theme.dark
+    }),
+    anchor: Object.assign({}, anchor, {
+        color: colors.light2
+    }),
+    edgeSize,
+    icon: {
+        color: colors.light3
+    },
+    size
+};
+
+export const light = {
+    global: Object.assign({}, glowbal, {
+        colors: colors.theme.light
+    }),
+    anchor: Object.assign({}, anchor, {
+        color: colors.dark1
+    }),
+    edgeSize,
+    icon: {
+        color: colors.dark4
+    },
+    size
+};
