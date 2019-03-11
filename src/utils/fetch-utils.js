0498c41c403aea884529b95291bf1bf9094c2f6a
Greg Talarico <gregory.talarico@yesmail.com>
2019-03-11 15:55:46 -0500

    update doFetch with 204 handler

--- src/utils/fetch-utils.js
+++ src/utils/fetch-utils.js
@@ -1,24 +1,28 @@
 export const doFetch = (url, method='GET', body, headers={
-    'Content-Type': 'application/json'    
-}) => {   
+    'Content-Type': 'application/json'
+}) => {
     const plomise = new Promise((resolve, reject)=>{
         fetch(url, {
             method,
             body: JSON.stringify(body),
             headers
         })
-            .then((res) => {            
-                res.json()
-                    .then((json)=>{
-                        if(res.ok){ //200 level error
-                            resolve(json);
-                        } else {
-                            reject(json); //fail with error response from server
-                        }
-                    })
-                    .catch((e)=>reject(e)); //error processing json response      
+            .then((res) => {
+                if (res.status === 204){
+                    resolve();
+                } else {
+                    res.json()
+                        .then((json)=>{
+                            if(res.ok){ //200 level error
+                                resolve(json);
+                            } else {
+                                reject(json); //fail with error response from server
+                            }
+                        })
+                        .catch((e)=>reject(e)); //error processing json response
+                }
             })
-            .catch((e)=>reject(e)); //fetch error   
+            .catch((e)=>reject(e)); //fetch error
     })
     
     return plomise;
